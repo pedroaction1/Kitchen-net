5d11c7ce25ff43c11e16b961c30148131e641bd7
pedroaction1 <78942152+pedroaction1@users.noreply.github.com>
2021-10-25 11:21:24 -0300

    Mudanças na tela principal + Criação básica da tela de login

--- src/App.js
+++ src/App.js
@@ -18,6 +18,36 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <div className="logo">
+        <img src={logo} alt="logo"></img>
+      
+        <p>
+          Kitchen Net Login Admin
+        </p>
+      </div>
+
+      <div className="Login"> 
+        <p>Login</p>
+
+        <div className="credenciais">
+          <div>
+            <label>Email:</label>
+            <input></input>
+          </div>
+
+          <div>
+            <label></label>
+            <input></input>
+          </div>
+        </div>
+      </div>
+
+
+      <button className="botao">
+      </button>
+
+      <div className="esquecisenha">Esqueci a Senha</div>
     </div>
   );
 }
