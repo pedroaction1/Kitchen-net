f26046a4bc64275cc883fa91315427279e1c9da5
pedroaction1 <pedro.action@hotmail.com>
2021-10-27 00:36:02 -0300

    Segundo Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import logo from './logo3.png';
 import './App.css';
 
 function App() {
   return (
+    <div className="BG">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="Container">
+          <div>
+            <img src ={logo} className="Logo"></img>
+          </div>
+
+          <h3>
+            Kitchen Net Login Admin
+          </h3>
+
+          <h3>
+            <strong>Login</strong>
+          </h3>
+          
+          <div className='LoginBox'> 
+
+            <label className='GoodLabel' ><strong>Email:</strong></label>
+
+            <div>
+              <input placeholder='Email' className='input'></input>
+            </div>
+
+            <label className='GoodLabel' ><strong>Senha:</strong></label>
+
+            <div>
+              <input placeholder='Senha' className='input'></input>
+            </div>
+
+          </div>
+
+          <div>
+            <button className='button'>Logar</button>
+          </div>
+
+          <h5>Esqueci a Senha</h5>
+
+        </div>
+      </div>
     </div>
   );
 }
